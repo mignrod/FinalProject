d1eee4bcda775aacd073fb6a18f0554c8e761ad6
fredy-tidy <fredy.castaneda.s21@gmail.com>
2025-08-02 00:04:35 -0600

    update the validation of field specification in device as a Object

--- middleware/validate.js
+++ middleware/validate.js
@@ -7,7 +7,7 @@ const saveDevice = (req, res, next) => {
     brand: "required|string",
     model: "required|string",
     specifications: "required",
-    price: "required",
+    price: "required|numeric",
     releaseDate: "required|date|date_format:YYYY-MM-DD"
   };
   validator(req.body, validationRule, {}, (err, status) => {
@@ -18,30 +18,34 @@ const saveDevice = (req, res, next) => {
         data: err,
       });
     } else {
+        // Validation of 'specifications' being a object
+        const specifications = req.body.specifications;
+        
+        // Validation is object and ( typeof null === 'object')
+        if (typeof specifications !== 'object' || specifications === null) {
+          return res.status(412).send({
+            success: false,
+            message: "Validation failed",
+            data: {
+              specifications: ["The specifications field must be an object."]
+            }
+          });
+        }
+        // Validation that field 'processor' is required in specifications and being a string.
+        if (typeof specifications.processor !== 'string' || specifications.processor.length === 0) {
+          return res.status(412).send({
+            success: false,
+            message: "Validation failed",
+            data: {
+              processor: ["The processor field is required and must be a string."]
+            }
+          });
+        }
       next();
     }
   });
 };
-/** 
-const saveClasses = (req, res, next) => {
-  const validationRule = {
-    course_code: "required|string",
-    course_name: "required|string",
-    rolled_students: "array|min:0",
-  };
-  validator(req.body, validationRule, {}, (err, status) => {
-    if (!status) {
-      res.status(412).send({
-        success: false,
-        message: "Validation failed",
-        data: err,
-      });
-    } else {
-      next();
-    }
-  });
-};
-*/
+
 module.exports = {
   saveDevice,
   
